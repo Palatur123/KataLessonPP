f20e141cd19b5eab3a3b7f2d637fbc498cf99218
Palatur <palatur.roman3008@mail.ru>
2022-02-11 18:29:15 +0300

    My var1

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,13 +1,26 @@
 public class HelloWorld {
- 
+
     private String message;
- 
-    public String getMessage() {
-        return message;
+
+    public boolean getMessage() {
+        return true;
     }
- 
+
     public void setMessage(String message) {
         this.message = message;
     }
-     
+
+
+    public static void main(String[] args) {
+        for (String ignored : args) {
+            HelloWorld pojo = new HelloWorld();
+            System.out.println(pojo);
+        }
+    }
+    public HelloWorld() {
+
+    }
+
+    public String toString() {
+        return this.message; }
 }
